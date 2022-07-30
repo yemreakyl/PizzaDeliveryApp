728495a09f780ca695a22594c2674c1e9b3dd938
Yunus emre Akyol <yemreakyl@gmail.com>
2022-07-30 15:09:03 +0300

    .

---
+++ PizzaDeliveryApp/Pages/Checkout/Thankyou.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PizzaDeliveryApp.Pages.Checkout
+{
+    public class ThankyouModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
