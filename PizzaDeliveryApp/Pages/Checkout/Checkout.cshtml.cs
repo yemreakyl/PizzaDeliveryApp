602665b3c93a258735bde94d4014f05478a9a799
Yunus emre Akyol <yemreakyl@gmail.com>
2022-08-01 15:56:49 +0300

    .

--- PizzaDeliveryApp/Pages/Checkout/Checkout.cshtml.cs
+++ PizzaDeliveryApp/Pages/Checkout/Checkout.cshtml.cs
@@ -3,10 +3,23 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PizzaDeliveryApp.Pages.Checkout
 {
+    [BindProperties(SupportsGet = true)]
     public class CheckoutModel : PageModel
     {
+        public string PizzaName { get; set; }
+        public float PizzaPrice { get; set; }
+        public string ImageTitle { get; set; }
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(PizzaName))
+            {
+                PizzaName = "Custom";
+            }
+
+            if (string.IsNullOrWhiteSpace(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
         }
     }
 }
--- PizzaDeliveryApp/Pages/Form/CustomPizza.cshtml.cs
+++ PizzaDeliveryApp/Pages/Form/CustomPizza.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzaDeliveryApp.Models;
 
 namespace PizzaDeliveryApp.Pages.Form
 {
     public class CustomPizzaModel : PageModel
     {
+        [BindProperty]
+        public PizzasModel Pizza{ get; set; }
+        public float PizzaPrice { get; set; }
         public void OnGet()
         {
+        }
+        public IActionResult OnPost() //Bu method kullanýcý seçimlerini yaptýktan sonra submit butonuna bastýðýnda çaðýrýlacak
+        {
+            PizzaPrice = Pizza.BasePrice;
+            if (Pizza.TomatoSauce) PizzaPrice += 1;
+            if (Pizza.Cheese) PizzaPrice += 2;
+            if (Pizza.Peperoni) PizzaPrice += 1;
+            if (Pizza.Mushroom) PizzaPrice += 2;
+            if (Pizza.Tuna) PizzaPrice += 1;
+            if (Pizza.Pineapple) PizzaPrice += 5;
+            if (Pizza.Ham) PizzaPrice += 3;
+            if (Pizza.Beef) PizzaPrice += 3;
+            return RedirectToPage("/Checkout/Checkout",new {Pizza.PizzaName,PizzaPrice });
         }
     }
 }
